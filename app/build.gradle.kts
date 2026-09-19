15a03c5836aaa89b7b5973c26968b654d1d69c6c
chandansp2012 <chandansp1947@gmail.com>
2026-09-19 16:55:54 +0530

    Add CameraX video pipeline dependencies
    Added CameraX video dependencies to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
+    // CameraX Engine & Video Pipeline
     val cameraVersion = "1.3.1"
     implementation("androidx.camera:camera-core:$cameraVersion")
     implementation("androidx.camera:camera-camera2:$cameraVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraVersion")
+    implementation("androidx.camera:camera-video:$cameraVersion")
     implementation("androidx.camera:camera-view:$cameraVersion")
 }
